9eb782ba9520399f152069838d9539480982202f
leocalmeida <leonardocaetanoalmeida@gmail.com>
2020-05-19 21:10:33 -0300

    Adicionando a string de conexao com o heroku

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3334",
+  baseURL: "https://omnistack07-backend.herokuapp.com/",
+  // baseURL: "http://localhost:3334",
 });
 
 export default api;
